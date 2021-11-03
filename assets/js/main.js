8dd76bb88b262c8079211d6a18bdd2dc2d5e2d34
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-11-03 08:52:46 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
